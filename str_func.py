b1a3e74c8ceed49c288b91728e5bb96611710868
sdanilov <srzdn1v@icloud.com>
2023-08-04 18:39:36 +0900

    add string upper && docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def string_title(value):
     """принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return value.title()
+
+def string_upper(value):
+    """заглавные первые буквы каждого слова в строке"""
+    return value.upper()
