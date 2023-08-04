85d1414c980f525dfaa63e7026969cceb9c70bba
sdanilov <srzdn1v@icloud.com>
2023-08-04 18:59:44 +0900

    add string upper && docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def string_title(value):
+    """Пывфывфывфывофылволфыв"""
     return value.title()
+
+
+def string_upper(value):
+    """принимает на вход строку и возвращает ее со всеми заглавными буквами"""
+    return value.upper()
