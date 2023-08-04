3018feed2e7b24a65834f30c24fa51a7c7f6063b
expexpexpexp <140854052+expexpexpexp@users.noreply.github.com>
2023-08-04 18:50:48 +0900

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,7 +1,2 @@
 def string_title(value):
-    """функция принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return value.title()
-
-def string_upper(value):
-    """заглавные первые буквы каждого слова в строке"""
-    return value.upper()
